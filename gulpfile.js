1a7f784baf542ed53e9e11dbaea6c81f73b78951
Anna Hrulckova <annahrulkova@yandex.ru>
2023-03-31 19:55:16 +0400

    добавлено уменьшение css файлов

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,16 @@
-const gulp = require(`gulp`)  
+const gulp = require('gulp');  /*загружает из node modules */
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const stripCssComments = require('gulp-strip-css-comments');
+
+gulp.task('minify-css', function() {
+  return gulp.src('./src/*.css') /*возвращает все css файлы */
+    .pipe(stripCssComments()) /*применяет методы */
+    .pipe(cleanCSS())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', gulp.series('minify-css'));
