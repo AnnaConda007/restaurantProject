792bbf8fcbdc0411472a512e5f82de3e0aeec3ed
Anna Hrulckova <annahrulkova@yandex.ru>
2023-04-02 20:14:31 +0400

    минимизировано в один файл

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const gulp = require('gulp');  /*загружает из node modules */
-
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const stripCssComments = require('gulp-strip-css-comments');
+const htmlmin = require('gulp-htmlmin');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+/*const imagemin = require('gulp-imagemin').default;*/
+
 gulp.task('minify-css', function() {
   return gulp.src('./src/*.css') /*возвращает все css файлы */
     .pipe(stripCssComments()) /*применяет методы */
@@ -12,27 +16,20 @@ gulp.task('minify-css', function() {
     }))
     .pipe(gulp.dest('./dist'));
 });
-gulp.task('default', gulp.series('minify-css'));
+ 
 
-
-const htmlmin = require('gulp-htmlmin');
 gulp.task('minify-html', () => {
   return gulp.src('src/*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('dist'));
 });
-gulp.task('default', gulp.series('minify-html'));
 
-
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
 gulp.task('minify-js', function() {
   return gulp.src('./src/js/*.js')
     .pipe(concat('main.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./dist/js'));
 });
-gulp.task('default', gulp.series('minify-js'));
 
 
 gulp.task('scripts', function() {
@@ -41,18 +38,14 @@ gulp.task('scripts', function() {
       .pipe(uglify())
       .pipe(gulp.dest('./dist/js'));
   });
-  gulp.task('default', gulp.series('scripts'));
  
-  
 gulp.task('styles', function() {
     return gulp.src('./src/css/*.css')
       .pipe(concat('main.min.css'))
       .pipe(cleanCSS())
       .pipe(gulp.dest('./dist/css'));
   });
-  gulp.task('default', gulp.series('styles'));
 
-  
 gulp.task('html', function() {
     return gulp.src('./src/*.html')
       .pipe(concat('index.html'))
@@ -60,9 +53,9 @@ gulp.task('html', function() {
       .pipe(gulp.dest('./dist'));
   });
   
-  gulp.task('default', gulp.series('html'));
+  gulp.task('default', gulp.series('minify-html', 'minify-css', 'minify-js', 'scripts','styles','html'));
 
 
-  const imagemin = require('gulp-imagemin').default;
+ 
 
   
