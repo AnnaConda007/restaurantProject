481d8210d9b364db14d4a7ae480dfa1dbc0f6fdb
Anna Hrulckova <annahrulkova@yandex.ru>
2023-04-03 22:51:47 +0400

    promiseAll

--- src/js/modules/JSON.js
+++ src/js/modules/JSON.js
@@ -1,38 +1,32 @@
 const barJSON = async (wrapSelector) => {
   const wrap = document.querySelector(wrapSelector)
-  let response = await fetch('./js/modules/JSON-files/bar-alco.JSON')
-  let result = await response.json()
-  for (let key in result) {
-    wrap.innerHTML += `    
-        <div class="product-wrap__product product ${result[key].class}"  id=${result[key].id}>
+  let responseAlco = await fetch('./js/modules/JSON-files/bar-alco.JSON')
+  const responseFree = await fetch('./js/modules/JSON-files/bar-free.JSON')
+  const allPromise = Promise.all([responseAlco, responseFree])
+  allPromise
+    .then((responses) =>
+      Promise.all(responses.map((response) => response.json())),
+    )
+    .then((jsonObjects) =>
+      jsonObjects.reduce((acc, cur) => acc.concat(cur), []),
+    )
+    .then((data) => {
+      for (let key in data) {
+        wrap.innerHTML += `  
+        <div class="product-wrap__product product ${data[key].class}"  id=${data[key].id}>
         <figure class="product__figure">
-            <img class="product__img" src=${result[key].src} alt="фото напитка">
-            <figcaption class="product__figcaption">${result[key].name}</figcaption>
+            <img class="product__img" src=${data[key].src} alt="фото напитка">
+            <figcaption class="product__figcaption">${data[key].name}</figcaption>
         </figure>
         <div class="product__order">
-            <h2 class="product__price">${result[key].price} &#x20bd</h2>
+            <h2 class="product__price">${data[key].price} &#x20bd</h2>
             <button class="button button--smal"> В корзину</button>
         </div>
-    </div>
-`
-  }
-
-  response = await fetch('./js/modules/JSON-files/bar-free.JSON')
-  result = await response.json()
-  for (let key in result) {
-    wrap.innerHTML += `    
-    <div class="product-wrap__product product ${result[key].class}"  id=${result[key].id}>
-    <figure class="product__figure">
-        <img class="product__img" src=${result[key].src} alt="фото напитка">
-        <figcaption class="product__figcaption">${result[key].name}</figcaption>
-    </figure>
-    <div class="product__order">
-        <h2 class="product__price">${result[key].price} &#x20bd</h2>
-        <button class="button button--smal"> В корзину</button>
-    </div>
-</div>
-`
-  }
+    </div>  
+           
+    `
+      }
+    })
 }
 
 export default barJSON
