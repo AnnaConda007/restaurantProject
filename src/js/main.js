2327db535333ae2f1966bdea723517ed9501bcf8
Anna Hrulckova <annahrulkova@yandex.ru>
2023-04-04 19:43:13 +0400

    удаление класса работает

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import menuBar from "./modules/menuBar.js"
 import filter from "./modules/filter.js"
-window.addEventListener("DOMContentLoaded", function () {
-    menuBar()
+window.addEventListener("DOMContentLoaded", async function () {
+   await menuBar()
     filter()
  
 })   
--- src/js/modules/filter.js
+++ src/js/modules/filter.js
@@ -2,16 +2,27 @@ const filter = () => {
     const allBtn = document.querySelectorAll(".menu-wrap__item")
     const allProduct = document.querySelectorAll(".product")
    
+    const removeClass = (elem, remClass)=>{
+        elem.forEach(item=>{
+            item.classList.remove(remClass)
+        })   
+    }
 
     allBtn.forEach(btn => {
+
+        
         btn.addEventListener("click", function () {
             allBtn.forEach(btn=>{
                 btn.classList.remove("active")
             })
             btn.classList.add("active")
             allProduct.forEach(product=>{
-                product.classList.remove("active-product")/*не работает */
+                product.classList.remove("active-product") 
             })
+            let activeClass = this.id
+            document.querySelectorAll(`.${activeClass}`).forEach(
+                product=>{product.classList.add("active-product")}
+            ) 
         })})
  
 
