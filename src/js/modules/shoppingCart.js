496aca468643c7c4e37f8b639191a4017075ef26
Anna Hrulckova <annahrulkova@yandex.ru>
2023-04-07 19:21:58 +0400

    товары добавляются и отрисовываются

--- src/js/modules/shoppingCart.js
+++ src/js/modules/shoppingCart.js
@@ -1,25 +1,52 @@
-const shoppingCart = () => {
+const shoppingCart = async() => {
     const shoppingArr = []
     const shopBtn = document.querySelectorAll(".button-add")
+    
     const takeData = (e) => {
         const сardProduct = e.target.closest(".product");
         const imgSrc = сardProduct.querySelector(".product__img").src;
         const name = сardProduct.querySelector(".product__figcaption").textContent;
         const price = сardProduct.querySelector(".product__price").textContent;
+        const btn = сardProduct.querySelector(".button");
+        btn.classList.add("added")
         const product = {
             img: imgSrc,
-            name: name,
+            nameProd: name,
             price: price
         };
         shoppingArr.push(product)
-        console.log(shoppingArr)
     }
+    const renderCart = ()=>{
+        document.querySelector(".background__products").innerHTML=``
+        shoppingArr.forEach(({ img, nameProd, price }) => { 
+            document.querySelector(".background__products").innerHTML +=`
+            <div class="background_unit unit">
+                <div class="unit__dish">
+                    <img class="unit__img" src=${img} alt="фото блюда">
+                    <h3 class="unit__name">${nameProd}</h3>
+                </div>
+                <div class="unit__price">
+                    <div class="unit__calculator calculator">
+                        <div class="calculator__buttons">
+                            <button class="calculator__button">+</button>
+                            <button class="calculator__button">-</button>
+                        </div>
+                        <h3 class="calculator__amount">2</h3>
+                    </div>
+                    <h3 class="unit__cost">${price}</h3>
+                </div>
+            </div>`
+        });
+       }
 
 
     shopBtn.forEach(btn => {
         btn.addEventListener("click", function (e) {
-            takeData(e)
-
+            if(!e.target.classList.contains("added")){
+                takeData(e)
+            }
+           renderCart()
+           
         })
     })
 
