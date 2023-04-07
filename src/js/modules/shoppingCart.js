afc17e317c42354ff7e56d222ff4951587a0afa3
Anna Hrulckova <annahrulkova@yandex.ru>
2023-04-07 18:18:12 +0400

    takeData(e) работает вне обработчика событий

--- src/js/modules/shoppingCart.js
+++ src/js/modules/shoppingCart.js
@@ -1,28 +1,24 @@
 const shoppingCart = () => {
     const shoppingArr = []
     const shopBtn = document.querySelectorAll(".button-add")
-
-
-
+    const takeData = (e) => {
+        const сardProduct = e.target.closest(".product");
+        const imgSrc = сardProduct.querySelector(".product__img").src;
+        const name = сardProduct.querySelector(".product__figcaption").textContent;
+        const price = сardProduct.querySelector(".product__price").textContent;
+        const product = {
+            img: imgSrc,
+            name: name,
+            price: price
+        };
+        shoppingArr.push(product)
+        console.log(shoppingArr)
+    }
 
 
     shopBtn.forEach(btn => {
         btn.addEventListener("click", function (e) {
-         
-         const takeData=()=>{
-            const сardProduct = e.target.closest(".product");
-            const imgSrc = сardProduct.querySelector(".product__img").src;
-            const name = сardProduct.querySelector(".product__figcaption").textContent;
-            const price = сardProduct.querySelector(".product__price").textContent;
-            const product = {
-                img: imgSrc,
-                name: name,
-                price: price
-            };
-            shoppingArr.push(product)
-            console.log(shoppingArr)
-         }   
-         takeData()
+            takeData(e)
 
         })
     })
