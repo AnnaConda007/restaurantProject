2287e1cd73003b1a4ec1d8ce034badc61977d249
Anna Hrulckova <annahrulkova@yandex.ru>
2023-04-07 15:45:13 +0400

    попытка подключить overlayscrollbars

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
-import {menuBar, filter, modal} from "./modules/index.js"
- 
+import {menuBar, filter, modal, scrol} from "./modules/index.js"
 window.addEventListener("DOMContentLoaded", async function () {
     await menuBar()
     filter()
@@ -15,4 +14,8 @@ window.addEventListener("DOMContentLoaded", async function () {
         modalClose: ".modal-form__form-background",
         display: "block"
     });
+    scrol()
+  
+
+
 })    
--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -1,5 +1,7 @@
 import menuBar from "./menuBar.js"
 import filter from "./filter.js"
 import modal from "./modal.js"
+import scrol from "./scrol.js";
 
-export {menuBar, filter, modal};
+
+export {menuBar, filter, modal, scrol};
