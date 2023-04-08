e8206bd03b6ba83ec2c61798ac5bf188ccee1c36
Anna Hrulckova <annahrulkova@yandex.ru>
2023-04-08 16:53:09 +0400

    переименованы функции

--- src/js/modules/shoppingCart.js
+++ src/js/modules/shoppingCart.js
@@ -1,25 +1,37 @@
 const shoppingCart = async () => {
     const shoppingArr = []
-    const shopBtn = document.querySelectorAll(".button-add")
     let product = {}
+
+    document.querySelectorAll(".button-add").forEach(btn => {
+        btn.addEventListener("click", function (e) {
+            takeData(e)
+            renderCart()
+            CountAmount()
+            renderSum()
+        })
+    })
+
     const takeData = (e) => {
-        const сardProduct = e.target.closest(".product");
-        const imgSrc = сardProduct.querySelector(".product__img").src;
-        const name = сardProduct.querySelector(".product__figcaption").textContent;
-        const price = сardProduct.querySelector(".product__price").textContent;
-        const idProduct = сardProduct.id
-        const btn = сardProduct.querySelector(".button");
-        btn.classList.add("added")
-        product = {
-            img: imgSrc,
-            nameProd: name,
-            price: price,
-            countPrice:price,
-            amount: 1,
-            id: idProduct
-        };
-        shoppingArr.push(product)
+        if (!e.target.classList.contains("added")) {
+            const сardProduct = e.target.closest(".product");
+            const imgSrc = сardProduct.querySelector(".product__img").src;
+            const name = сardProduct.querySelector(".product__figcaption").textContent;
+            const price = сardProduct.querySelector(".product__price").textContent;
+            const idProduct = сardProduct.id
+            const btn = сardProduct.querySelector(".button");
+            btn.classList.add("added")
+            product = {
+                img: imgSrc,
+                nameProd: name,
+                price: price,
+                countPrice: price,
+                amount: 1,
+                id: idProduct
+            };
+            shoppingArr.push(product)
+        }
     }
+
     const renderCart = () => {
         document.querySelector(".background__products").innerHTML = ``
         shoppingArr.forEach(({ img, nameProd, price, amount, id }) => {
@@ -40,11 +52,10 @@ const shoppingCart = async () => {
                     <h3 class="unit__cost">${price}</h3>
                 </div>
             </div>`
-
         });
     }
 
-    const calculatorBtn = () => {
+    const CountAmount = () => {
         const plusBtns = document.querySelectorAll('.plus')
         plusBtns.forEach(btn => {
             btn.addEventListener("click", (e) => {
@@ -53,8 +64,8 @@ const shoppingCart = async () => {
                 product.amount++
                 const amountText = e.target.closest(".calculator");
                 amountText.querySelector(".calculator__amount").innerHTML = product.amount
-                count(e)
-                renderSum()  
+                totalPrice(e)
+                renderSum()
             })
         })
         const minusBtns = document.querySelectorAll('.minus')
@@ -64,40 +75,39 @@ const shoppingCart = async () => {
                 const product = shoppingArr[productIndex];
                 if (product.amount != 0) {
                     product.amount = product.amount - 1
-                    count(e)
-                    renderSum()  
+                    totalPrice(e)
+                    renderSum()
                 }
                 const amountText = e.target.closest(".calculator");
                 amountText.querySelector(".calculator__amount").innerHTML = product.amount
-                removeUnitProduct (e)
+                removeUnitProduct(e)
             })
         })
     }
-   
-const count = (e)=>{
-    const productIndex = shoppingArr.findIndex(p => p.id === e.target.dataset.articul);
-    const product = shoppingArr[productIndex];
-     const unit = e.target.closest(".unit");
-     const price = unit.querySelector(".unit__cost")
-      price.innerHTML=parseFloat(product.price)*product.amount
-      product.countPrice=parseFloat(product.price)*product.amount
- 
-   }
 
-const renderSum = ()=>{
-    let sum = shoppingArr.reduce((acc, cur) => acc + parseFloat(cur.countPrice), 0);
-    document.querySelector(".background__sum").innerHTML=sum
-}
-
-
-  
-
-    const removeUnitProduct = (e)=>{
+    const totalPrice = (e) => {
         const productIndex = shoppingArr.findIndex(p => p.id === e.target.dataset.articul);
-    const product = shoppingArr[productIndex];
-        if (product.amount<1) {
-            shoppingArr.splice(productIndex, 1);
+        const product = shoppingArr[productIndex];
+        const unit = e.target.closest(".unit");
+        const price = unit.querySelector(".unit__cost")
+        price.innerHTML = parseFloat(product.price) * product.amount
+        product.countPrice = parseFloat(product.price) * product.amount
 
+    }
+
+    const renderSum = () => {
+        let sum = shoppingArr.reduce((acc, cur) => acc + parseFloat(cur.countPrice), 0);
+        document.querySelector(".background__sum").innerHTML = sum
+    }
+
+
+
+
+    const removeUnitProduct = (e) => {
+        const productIndex = shoppingArr.findIndex(p => p.id === e.target.dataset.articul);
+        const product = shoppingArr[productIndex];
+        if (product.amount < 1) {
+            shoppingArr.splice(productIndex, 1);
             const cardProduct = e.target.closest(".unit");
             const idBtn = cardProduct.getAttribute('data-articul')
             document.getElementById(idBtn).querySelector(".button").classList.remove("added")
@@ -106,16 +116,7 @@ const renderSum = ()=>{
 
     }
 
-    shopBtn.forEach(btn => {
-        btn.addEventListener("click", function (e) {
-            if (!e.target.classList.contains("added")) {
-                takeData(e)
-            }
-            renderCart()
-            calculatorBtn()
-            renderSum()
-        })
-    })
+
 
 
 }
