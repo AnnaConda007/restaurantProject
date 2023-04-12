257805e7fa3d17ecd21b9cc6f03faf25c2b8e25d
Anna Hrulckova <annahrulkova@yandex.ru>
2023-04-12 14:38:11 +0400

    init

--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -2,5 +2,6 @@ import menuBar from "./menuBar.js"
 import filter from "./filter.js"
 import modal from "./modal.js" 
 import shoppingCart from "./shoppingCart.js";
+import send from "./sending.js";
 
-export {menuBar, filter, modal, shoppingCart };
+export {menuBar, filter, modal, shoppingCart, send };
