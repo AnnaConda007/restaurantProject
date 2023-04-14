583b7749df3a5bf2b67f23fd8eeb9592844e89cf
Anna Hrulckova <annahrulkova@yandex.ru>
2023-04-15 00:45:09 +0400

    работает

--- src/js/sendForm.js
+++ src/js/sendForm.js
@@ -1,24 +1,22 @@
 const sendForm = () => {
 
-    function valid(inputType, regex, upper=false, time="false") {
+    function valid(inputType, regex, name=false, phone=false, time=false) {
         inputType.addEventListener("input", () => {
             let newValue = inputType.value.replace(regex, "");
-           if(upper==true) {
+           if(name==true) {
             newValue = newValue.charAt(0).toUpperCase() + newValue.slice(1);}
 
-            if(time==true){
-                newValue = newValue.slice(0,2) + " " + newValue.slice(2);
+            if(phone==true){
+                newValue = newValue.replace(/(\d{3})(\d{3})(\d{2})(\d{2})/, "$1 $2 $3 $4");
             }
-
-
             inputType.value = newValue;
         });
     }
 
-valid(document.getElementById("name"),/[^а-яА-Я]/g, true)
-valid(document.getElementById("phone"),/[^0-9]/g)
-valid(document.getElementById("guests"),/[^0-9]/g)
-valid(document.getElementById("time"),/[^0-9]/g, false,true)
+valid(document.getElementById("name"),/[^а-яА-Я]/g, true, false, false)
+valid(document.getElementById("phone"),/[^0-9]/g,false,true, false)
+valid(document.getElementById("guests"),/[^0-9]/g, false, false, false)
+valid(document.getElementById("time"),/[^0-9]/g, false,false,true)
 
 }
 
