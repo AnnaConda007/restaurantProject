c32beb4934497564dd3121d4c4f91a0b91c927a2
Anna Hrulckova <annahrulkova@yandex.ru>
2023-04-15 01:21:21 +0400

    убрала sendForm

--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -2,6 +2,5 @@ import menuBar from "./menuBar.js"
 import filter from "./filter.js"
 import modal from "./modal.js" 
 import shoppingCart from "./shoppingCart.js";
-import sendForm from "../sendForm.js";
 
-export {menuBar, filter, modal, shoppingCart, sendForm };
+export {menuBar, filter, modal, shoppingCart };
