f8751fcb62d2e5c59fe435e84aad1438e44983cf
Anna Hrulckova <annahrulkova@yandex.ru>
2023-04-15 01:08:14 +0400

    отформатировано

--- src/js/sendForm.js
+++ src/js/sendForm.js
@@ -10,7 +10,7 @@ const sendForm = () => {
                 newValue = newValue.replace(/(\d{3})(\d{3})(\d{2})(\d{2})/, "$1 $2 $3 $4");
             }
 
-            if (time == true){
+            if (time == true) {
                 newValue = newValue.replace(/(\d{2})(\d{2})/, "$1 $2")
             }
             inputType.value = newValue;
@@ -28,15 +28,15 @@ const sendForm = () => {
 
     valid({
         inputType: document.getElementById("phone"),
-        regex:  /[^0-9]/g,
+        regex: /[^0-9]/g,
         name: false,
         phone: true,
         time: false
     })
- 
+
     valid({
         inputType: document.getElementById("guests"),
-        regex:  /[^0-9]/g,
+        regex: /[^0-9]/g,
         name: false,
         phone: false,
         time: false
@@ -44,12 +44,15 @@ const sendForm = () => {
 
     valid({
         inputType: document.getElementById("time"),
-        regex:  /[^0-9]/g,
+        regex: /[^0-9]/g,
         name: false,
         phone: false,
         time: true
     })
 
+    document.querySelector(".").addEventListener("click", (e) => {
+        e.preventDefault()
+    })
 }
 
 export default sendForm
