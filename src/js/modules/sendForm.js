af62d2b9e3d6ade786d4bfd2b0426081daef062e
Anna Hrulckova <annahrulkova@yandex.ru>
2023-04-15 21:48:05 +0400

    fetch

--- src/js/modules/sendForm.js
+++ src/js/modules/sendForm.js
@@ -49,9 +49,22 @@ const sendForm = () => {
         time: true
     })
 
-    document.querySelector(".form__button").addEventListener("submit", (e) => {
-   
-    })
+    const handleSubmit = (event) => {
+        event.preventDefault();
+  
+        const myForm = event.target;
+        const formData = new FormData(myForm);
+  
+        fetch("/", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: new URLSearchParams(formData).toString(),
+        })
+          .then(() => console.log("Form successfully submitted"))
+          .catch((error) => alert(error));
+      };
+  
+      document.querySelector("form").addEventListener("submit", handleSubmit);
 }
 
 export default sendForm
