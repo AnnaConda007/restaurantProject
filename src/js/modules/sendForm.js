e2deaa72571ce9a7bb6d4c278aee04c6b8973a5b
Anna Hrulckova <annahrulkova@yandex.ru>
2023-04-15 14:53:28 +0400

    ===

--- src/js/modules/sendForm.js
+++ src/js/modules/sendForm.js
@@ -1,24 +1,24 @@
 const sendForm = () => {
 
-    function valid({ input, regex, name, phone, time }) {
-        input.addEventListener("input", () => {
-            let newValue = input.value.replace(regex, "");
-            if (name == true) {
+    function valid({ inputId, regex, name, phone, time }) {
+        inputId.addEventListener("input", () => {
+            let newValue = inputId.value.replace(regex, "");
+            if (name === true) {
                 newValue = newValue.charAt(0).toUpperCase() + newValue.slice(1);
             }
-            if (phone == true) {
+            if (phone === true) {
                 newValue = newValue.replace(/(\d{3})(\d{3})(\d{2})(\d{2})/, "$1 $2 $3 $4");
             }
 
-            if (time == true) {
+            if (time === true) {
                 newValue = newValue.replace(/(\d{2})(\d{2})/, "$1 $2")
             }
-            input.value = newValue;
+            inputId.value = newValue;
         });
     }
 
     valid({
-        input: document.getElementById("name"),
+        inputId: document.getElementById("name"),
         regex: /[^а-яА-Я]/g,
         name: true,
         phone,
@@ -26,7 +26,7 @@ const sendForm = () => {
     })
 
     valid({
-        input: document.getElementById("phone"),
+        inputId: document.getElementById("phone"),
         regex: /[^0-9]/g,    
         name,
         phone: true,
@@ -34,7 +34,7 @@ const sendForm = () => {
     })
 
     valid({
-        input: document.getElementById("guests"),
+        inputId: document.getElementById("guests"),
         regex: /[^0-9]/g,
         name,
         phone,
@@ -42,7 +42,7 @@ const sendForm = () => {
     })
 
     valid({
-        input: document.getElementById("time"),
+        inputId: document.getElementById("time"),
         regex: /[^0-9]/g,
         name,
         phone,
